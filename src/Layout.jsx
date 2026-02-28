f64456f60c16b60de32523290e8b306bc708e49a
Peter Mark Dela Cruz <peter@thinklab.ph>
2026-02-28 05:10:41 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import { Button } from "@/components/ui/button";
