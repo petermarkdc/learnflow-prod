57cec5a0890ed92af57b47b00ce95dbd264acf34
Peter Mark Dela Cruz <peter@thinklab.ph>
2026-02-28 06:16:26 +0000

    File changes

--- src/pages/CourseEditor.jsx
+++ src/pages/CourseEditor.jsx
@@ -298,15 +298,9 @@ export default function CourseEditor() {
                       <SelectValue placeholder="Select category" />
                     </SelectTrigger>
                     <SelectContent>
-                      <SelectItem value="web-development">Web Development</SelectItem>
-                      <SelectItem value="programming-basics">Programming Basics</SelectItem>
-                      <SelectItem value="devops">DevOps</SelectItem>
-                      <SelectItem value="data-science">Data Science</SelectItem>
-                      <SelectItem value="mobile-development">Mobile Development</SelectItem>
-                      <SelectItem value="robotics">Robotics</SelectItem>
-                      <SelectItem value="arduino">Arduino</SelectItem>
-                      <SelectItem value="raspberry-pi">Raspberry Pi</SelectItem>
-                      <SelectItem value="other">Other</SelectItem>
+                      {ALL_CATEGORIES.map(cat => (
+                        <SelectItem key={cat.value} value={cat.value}>{cat.label}</SelectItem>
+                      ))}
                     </SelectContent>
                   </Select>
                 </div>
