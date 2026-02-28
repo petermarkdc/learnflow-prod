fa1d5f839920ffe84f0c55756ab4ca8d29a64e08
Peter Mark Dela Cruz <peter@thinklab.ph>
2026-02-28 12:52:11 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { 
   GraduationCap, Home, LayoutDashboard, 
-  LogOut, Menu, X, Plus, BookOpen, Tag, UserCircle
+  LogOut, Menu, X, Plus, BookOpen, Tag, UserCircle, BarChart2
 } from 'lucide-react';
 import { cn } from "@/lib/utils";
 import {
